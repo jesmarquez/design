224b1d46145bda4035afb4fa0005b258fea2b1e7
jesmarquez <jesmarquezacevedo@yahoo.es>
2016-11-16 16:49:32 -0500

    initial installation

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res){
+	res.render('index');
+
+})
+
+app.listen(3000, function(err){
+
+	if(err) return console.log('Hubo error!'), process.exit(1);
+
+	console.log('Escuchando en el puerto 3000!');
+})
