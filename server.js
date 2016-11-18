5e1c95186e22e085bf348cc685c6c18a69802a83
cituao <jesmarquezacevedo@yahoo.es>
2016-11-18 17:32:33 -0500

    add pagejs and home

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ app.use(express.static('public'));
 
 app.get('/', function(req, res){
 	res.render('index');
+})
 
+app.get('/signup', function(req, res){
+	res.render('index');
 })
 
 app.listen(3000, function(err){
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert('Hola');
+var page = require('page');
+
+var main = document.getElementById('ofertas');
+
+page('/', function(ctx, next){
+    //main.innerHTML = 'Home';
+})
+
+
+page('/signup', function(ctx, next){
+    //main.innerHTML = 'Signup';
+})
+
+page();	
