821b85735510fd901ac5e5a5da38be709fbbfcab
cituao <jesmarquezacevedo@yahoo.es>
2016-12-01 09:34:10 -0500

    adding title

--- src/signup/index.js
+++ src/signup/index.js
@@ -1,8 +1,10 @@
 var page = require('page');
 var empty = require('empty-element');
 var template = require('./template');
+var title = require('title');
 
 page('/signup', function(ctx, next){
+	title('Jobby - Signup');
 	var mainSection = document.getElementById('main-section');
 	var secondarySection = document.getElementById('secondary-section');
 
